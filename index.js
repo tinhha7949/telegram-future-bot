74379c3cbe31c7032e3c86db4e50e44f5f0cd0d7
tinhha7949 <tinhha7949@gmail.com>
2026-03-23 13:04:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -244,8 +244,8 @@ let trendStrongHTF = Math.abs(ema20_1h - ema50_1h)/price > 0.001
     let side=null, score=0, type="MAIN"
     if(trendLong){ side="LONG"; score+=50 }
     if(trendShort){ side="SHORT"; score+=50 }
-    if(!isBacktest && side==="SHORT" && price < ema20 *1.002) return null
-    if(!isBacktest && side==="LONG" && price > ema20 *0.998) return null
+  //  if(!isBacktest && side==="SHORT" && price < ema20 *1.002) return null
+   // if(!isBacktest && side==="LONG" && price > ema20 *0.998) return null
     if(side==="LONG" && bosConfirmUp && pullbackLong) score+=40
     if(side==="SHORT" && bosConfirmDown && pullbackShort) score+=40
     if(side==="LONG" && r>55 && r<65) score+=12
