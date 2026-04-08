22d2f9595b7e238b9ae454e57a5f7f8f7c780335
tinhha7949 <tinhha7949@gmail.com>
2026-04-09 05:22:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const LIMIT_1H  = 200 //100
 
 const SCORE_THRESHOLD = 90 // 110
 const EARLY_THRESHOLD = 55  // 60
-const RR_THRESHOLD = 1.3 // 1.3 hoặc 1.4 nếu muốn 
+const RR_THRESHOLD = 1.25 // 1.3 hoặc 1.4 nếu muốn 
 
 const RISK_PER_TRADE = 0.01
 const ACCOUNT_BALANCE = 1000
