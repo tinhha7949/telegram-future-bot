30fd8ff2a72c5b939498162c82ef309e44272881
tinhha7949 <tinhha7949@gmail.com>
2026-05-10 20:50:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -138,8 +138,8 @@ const LIMIT_1H  = 200 //100
 const SCORE_THRESHOLD = 35 // 110
 const RR_THRESHOLD = 1.3 // 1.3 hoặc 1.4 nếu muốn 
 
-const RISK_PER_TRADE = 0.01
-const POSITION_SIZE_PERCENT = 0.05 // 5% vốn vào lệnh
+const RISK_PER_TRADE = 0.02
+const POSITION_SIZE_PERCENT = 0.1 // 0.05 5% vốn vào lệnh
 let ACCOUNT_BALANCE = 0
 const MIN_VOL_15M = 60000 // 100000 hoặc  nếu rác
 
